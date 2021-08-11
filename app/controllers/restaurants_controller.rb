f84c44cc9d3ec35d6195134f5f2ba889665b47bd
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-11 13:39:40 +0200

    models completed

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,15 @@
 class RestaurantsController < ApplicationController
+  # before_action :find_restaurant
+ def index
+   @restaurants = Restaurant.all
+ end
+
+ def show
+ end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
