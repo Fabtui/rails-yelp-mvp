c1d5555312f4840ba11f88a27a04e680e7951852
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-11 14:45:22 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants, only: [ :new, :create, :show, :index ] do
-    resources :reviews, only: [ :new, :create]
+    resources :reviews, only: [ :new, :create, :index]
   end
-  resources :reviews, only: [:show, :index]
+  # resources :reviews, only: [:show, ]
 end
